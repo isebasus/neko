460a0be3b75f1d45ff5afb102f141074078c209f
Sebastian Carbonero <isebasus@gmail.com>
2021-04-06 20:03:41 -0700

    cat bot uwu

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,32 +1,58 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot
 {
     public class Program
     {
         private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
 	
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
+            string token = "ODI4NDkxMjQyNjI3MjY4NjY4.YGqWmA.6t0MD_cBBBJCxTagcgVq6S2cZYQ";
+            
             _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+            
             _client.Log += Log;
             await _client.LoginAsync(TokenType.Bot, 
-                Environment.GetEnvironmentVariable("ODI4NDkxMjQyNjI3MjY4NjY4.YGqWmA.6t0MD_cBBBJCxTagcgVq6S2cZYQ"));
+                token);
             await _client.StartAsync();
 		
             // Block this task until the program is closed.
             await Task.Delay(-1);
         }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private Task HandleCommandAsync(SocketMessage arg)
+        {
+            return Task.CompletedTask;
+        }
+        
         private Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
+        
+        
     }
 }
