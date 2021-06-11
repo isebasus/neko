a3f035fda30cdfd95c686a5746efa5103636208a
Sebastian Carbonero <isebasus@gmail.com>
2021-06-11 12:18:01 -0700

    roles

--- CatGirl/Program.cs
+++ CatGirl/Program.cs
@@ -25,7 +25,7 @@ namespace DiscordBot
 
         public async Task MainAsync()
         {
-            string token = "ODI4NDkxMjQyNjI3MjY4NjY4.YGqWmA.bU35oW0DDYzMHsp3tgL2ZwVKkfY";
+            string token = "";
             
             _client = new DiscordSocketClient();
             _commands = new CommandService();
