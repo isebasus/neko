bfded26c6d5e92168cdf13714ec23a3428478377
Sebastian <isebasus@gmail.com>
2021-11-22 12:51:33 -0800

    lol again

--- catgirl-bot/Catgirl.cs
+++ catgirl-bot/Catgirl.cs
@@ -22,7 +22,7 @@ namespace catgirl_bot
 
         public async Task MainAsync()
         {
-            string token = "ODU2MjUyNDY1OTE2Njc0MTA4.YM-VQA.tB9GkstLoDZ-r9iXZwO68A0jkYA";
+            string token = "";
 
             _client = new DiscordSocketClient();
             _commands = new CommandService();
